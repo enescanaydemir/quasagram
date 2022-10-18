dc6c136eee2e94b9271210675b4174bdb8918118
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2022-10-18 16:35:07 +0300

    Layout, Pages & Routes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,39 +1,20 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn to="/camera" class="large-screen-only q-mr-sm" icon="eva-camera-outline" size="18px" flat round dense />
+        <q-separator class="large-screen-only" vertical spaced />
+        <q-toolbar-title class="text-grand-hotel">
+          Quasagram
+        </q-toolbar-title>
+        <q-btn to="/" class="large-screen-only" icon="eva-home-outline" size="18px" flat round dense />
       </q-toolbar>
     </q-header>
 
-    <q-footer
-      class="bg-white"
-      bordered
-      >
-      <q-tabs
-        class="text-grey-10"
-        active-color="primary"
-        indicator-color="transparent"
-      >
-        <q-route-tab
-          to="/"
-          icon="eva-home-outline"
-        />
-        <q-route-tab
-          to="/camera"
-          icon="eva-camera-outline"
-        />
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs class="text-grey-10" active-color="primary" indicator-color="transparent">
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
       </q-tabs>
     </q-footer>
 
@@ -44,17 +25,21 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
+export default {
   name: "MainLayout",
 
-  setup() {
+  data() {
     return {};
   },
-});
+};
 </script>
+
 <style lang="sass">
+  .q-toolbar
+      height: 70px
+  .q-toolbar__title
+    font-size: 30px
+      text-align: center
   .q-footer
       .q-tab__icon
           font-size: 30px
